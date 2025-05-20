3b669b8840026faa43038c7d04cd90c49ff7e1ae
Khalid <khalidfarajdev@gmail.com>
2025-05-20 18:39:33 +0300

    01. Prepare the project then adding Routing and design the layout

---
+++ src/components/common/Header/Header.tsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import { Badge } from 'react-bootstrap'
+import styles from './styles.module.css'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import HeaderBasker from '../../eCommerce/HeaderBasket/HeaderBasker';
+const { headerContainer, headerLogo } = styles;
+
+
+export const Header = () => {
+  return (
+<header>   
+     <div className={headerContainer}>
+    <h1 className={headerLogo}><span>our</span> <Badge bg='info'>Ecom</Badge></h1>
+    <HeaderBasker/>
+    </div>
+     <Navbar expand="lg" className="bg-body-tertiary" bg='dark' data-bs-theme='dark'>
+      <Container>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="categories">Categories</Nav.Link>
+            <Nav.Link as={NavLink} to="about-us">About Us</Nav.Link>
+          </Nav>
+           <Nav>
+            <Nav.Link as={NavLink} to="login">Login</Nav.Link>
+            <Nav.Link as={NavLink} to="register">Register</Nav.Link>
+
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
+  )
+}
